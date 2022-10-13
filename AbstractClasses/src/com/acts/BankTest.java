95bb21afe89c20ee41816f3a3ee3521a48be87a4
Atharva <atharvachowkidar1@gmail.com>
2022-10-13 12:31:36 +0530

    changes done by atharva on 13 oct 2022

--- AbstractClasses/src/com/acts/BankTest.java
+++ AbstractClasses/src/com/acts/BankTest.java
@@ -1,24 +1,39 @@
 package com.acts;
+import java.util.Scanner;
+
 import com.acts.derivedclass.*;
 import com.acts.operations.*;
 
 public class BankTest {
 	public static void main(String[] args) {
-	Bank b = new SBIBank();
-	int c = b.rateOfInterest();
-	System.out.println(c);
-	
-	b = new ICICIBank();
-	System.out.println(b.rateOfInterest());
-	
+		Scanner sc = new Scanner(System.in) ;
+		Bank b = new SBIBank();
+		int option =0 ;
+		
+		do {
+			System.out.println("Which bank Rate of intrest you want to know? \n 1. SBI Bank \n 2. ICICI Bank \n 0. Exit ");
+			option = sc.nextInt() ;
+			if(option==0)
+				break ;
+			if(option==1) {
+				int c = b.rateOfInterest();
+				System.out.println(c);
+			}else if(option==2){
+				b = new ICICIBank();
+				System.out.println(b.rateOfInterest());
+			}else {
+				System.out.println("You have Enterd wrong choice please try again");
+			}
+		}while(option!=0) ;
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
 }
--- Complex.java
+++ Complex.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
 
 public class Complex {
 	private int real; 
 	private int img ;
-	public Complex( ) {  //constructor
-
-	} 
+	Scanner sc = new Scanner(System.in) ;
+	
+	
+	public Complex() {
+		System.out.println("Enter real value ");
+		real = sc.nextInt();
+		System.out.println("Enter img value");
+		img = sc.nextInt() ;
+	}
 	public Complex(int real,int img ) {	//para constructor
 		this.real=real;
 		this.img=img;
 	}
-	public void display()  {                                       //class method
-		System.out.println("complex number is ="+real+"j"+img);
+	public void display()  {                                       //class method 3+j4
+		System.out.println("complex number is ="+this.real+" j"+this.img);
 		}
+	public int getreal() {
+		return this.real ;
+	}
+	public int getimg() {
+		return this.img ;
+	}
+	public void add(Complex cn1) {
+		this.real = this.real+cn1.getreal() ;
+		this.img= this.img+cn1.getimg() ;
+	}
+	public String toString() {{
+		return "complex number is ="+this.real+" j"+this.img ;
+	}
+	
 	}
