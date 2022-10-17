210c333df06d5d5fa5512c3d996a7f9cec9ffc14
Atharva <atharvachowkidar1@gmail.com>
2022-10-18 00:33:29 +0530

    this is updated by atharva

---
+++ Day17_oct/src/com/acts/pet/AuthenticationExce.java
@@ -0,0 +1,9 @@
+package com.acts.pet;
+
+public class AuthenticationExce extends Exception {
+
+	public AuthenticationExce(String message) {
+		super(message);
+	}
+
+}
--- Day17_oct/src/com/acts/pet/Order.java
+++ Day17_oct/src/com/acts/pet/Order.java
@@ -1,9 +1,42 @@
 package com.acts.pet;
 
 public class Order {
+private static int orderId_Start =101 ;
 private int orderId;
- private int petIdO;
+ private int petId;
  private int quantity;
  private String status;
+public Order( int petId, int quantity, String status) {
+	super();
+	this.orderId = orderId_Start++;
+	this.petId = petId;
+	this.quantity = quantity;
+	this.status = status;
+}
+public int getOrderId() {
+	return orderId;
+}
+
+public int getPetId() {
+	return petId;
+}
+
+public int getQuantity() {
+	return quantity;
+}
+public void setQuantity(int quantity) {
+	this.quantity = quantity;
+}
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+@Override
+public String toString() {
+	return "Order [orderId=" + orderId + ", petId=" + petId + ", quantity=" + quantity + ", status=" + status + "]";
+}
+ 
  
 }
